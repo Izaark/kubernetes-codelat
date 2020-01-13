f972f3f0873551c0fc9208b38a328cfb31d9ce3e
izaark <izaark200@gmail.com>
2020-01-12 19:45:42 -0600

    upgrade deployment, upgrade views

--- kubernetes_01/users/views.py
+++ kubernetes_01/users/views.py
@@ -1,12 +1,16 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+
 class HelloKubernetesApiView(APIView):
 	def get(self, request, *args, **kwargs):
 		obj_user = {
 				'name': 'Isaac',
 				'username': 'Izaark',
-				'github': 'https://github.com/Izaark'
+				'github': 'https://github.com/Izaark',
+				'country': 'Mexico',
+				'city': 'Mexico City',
+				'linkedin': 'https://www.linkedin.com/in/isaacflm/'
 		}
 		response_data = {'message': 'success', 'user': obj_user}
 		return Response(response_data)
